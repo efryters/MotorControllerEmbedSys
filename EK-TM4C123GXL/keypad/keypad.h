1e3b13b93bc1d905030f4c0a841846c9ac163b7d
DanCee73 <danielkpcordone@gmail.com>
2018-03-21 00:14:01 -0400

    Added setup of GPIO pins and defined a truth table. todo: configure pins as pull-up using gpioPadConfigset().

--- EK-TM4C123GXL/keypad/keypad.h
+++ EK-TM4C123GXL/keypad/keypad.h
@@ -1,6 +1,24 @@
 #ifndef KEYPAD_H_
 #define KEYPAD_H_
 
+// Truth Table
+#define Zero 0x21
+#define One 0x48
+#define Two 0x28
+#define Three 0x18
+#define Four 0x44
+#define Five 0x24
+#define Six 0x14
+#define Seven 0x42
+#define Eight 0x22
+#define Nine 0x12
+#define Asterisk 0x41
+#define Pound 0x11
+
+// Method Prototype
+void Init_port(void);
+void Define_pins(void);
+
 
 
 
